f4bf1257d04e324392842e63f3daa71df99ca0a5
Qianming <qianming.huang@cern.ch>
2020-02-11 16:21:36 +0100

    jes source

---
+++ PKUTreeMaker/test/submit2.sh
@@ -0,0 +1,8 @@
+crab submit -c ./Wcrab/crab3_analysisWWA.py
+crab submit -c ./Wcrab/crab3_analysisWZQQ.py
+crab submit -c ./Wcrab/crab3_analysisZZQQ.py
+crab submit -c ./Wcrab/crab3_analysisWZA.py
+crab submit -c ./Wcrab/crab3_analysisWWQQ1.py
+crab submit -c ./Wcrab/crab3_analysisWWQQ2.py
+crab submit -c ./Wcrab/crab3_analysisWWQQ3.py
+
