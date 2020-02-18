bca7f283ac215b56e5d4ea47a215f8e4c16db0d1
Qianming <qianming.huang@cern.ch>
2020-02-18 14:58:53 +0100

    jes source

--- PKUTreeMaker/test/Wcrab/crab3_analysisWAJJ.py
+++ PKUTreeMaker/test/Wcrab/crab3_analysisWAJJ.py
@@ -10,14 +10,16 @@ config.JobType.inputFiles = ['Summer16_23Sep2016V4_MC_L1FastJet_AK4PFchs.txt','S
 # Name of the CMSSW configuration file
 config.JobType.psetName    = 'analysis.py'
 config.JobType.allowUndistributedCMSSW = True
-config.JobType.maxMemoryMB = 5000
+config.JobType.maxMemoryMB = 2500
 
 config.section_("Data")
 config.Data.inputDataset = '/LNuAJJ_EWK_MJJ-120_13TeV-madgraph-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 #config.Data.splitting = 'FileBased'
-config.Data.splitting = 'Automatic'
+#config.Data.splitting = 'Automatic'
 #config.Data.unitsPerJob = 1
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 10
 config.Data.totalUnits = -1
 config.Data.outLFNDirBase = '/store/group/phys_jetmet/qihuang/'
 #config.Data.outLFNDirBase = '/store/user/qihuang/'
